38da8db1ee8c26d3aff7e7b963f27c091a96dc9e
Rachna <sunny@Macbook.local>
2026-03-26 20:51:41 -0400

    added sout

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <100 ; i++) {
-            System.out.println("Hello");
-            System.out.println("Loops");
-        }
+        System.out.println("Hello this is Manya.....");
+        System.out.println("Hello this is Manya.....");
+        System.out.println("Hello this is Manya.....");
+        System.out.println("Hello this is Manya.....");
+        System.out.println("Hello this is Manya.....");
+        System.out.println("Hello this is Manya.....");
+        System.out.println("Hello this is Manya.....");
+        System.out.println("Hello this is Manya.....");
     }
 }
