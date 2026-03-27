f927138f8a5428dbec2fe63e5e250cd43ea96b94
Sunny Singh <sunny@Sunnys-Laptop.local>
2026-03-26 21:03:17 -0400

    removed lines

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <100 ; i++) {
-            System.out.println("Hello");
-            System.out.println("Loops");
-        }
+        System.out.println("Removed lines");
     }
 }
