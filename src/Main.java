40041e0b3468371d9b413509ce8d03495e0619b0
Sunny Singh <sunny@Sunnys-Laptop.local>
2026-03-26 20:24:54 -0400

    added for loop in main file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("From feature_nikhil");
-        System.out.println("just checking in feature_branch");
+        for (int i = 0; i <100 ; i++) {
+            System.out.println("Hello");
+            System.out.println("Loops");
+        }
     }
 }
