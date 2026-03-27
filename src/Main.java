fd80dc33678a1fb9bf4953d450dbd57529234d41
theLazyProgrammer <electro_nik_54@hotmail.com>
2026-03-26 20:55:19 -0400

    updated code and added an infinet loop

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i <100 ; i++) {
             System.out.println("Hello");
-            System.out.println("Loops");
+            System.out.println("i was here!!");
+        }
+
+        boolean exit = false;
+        while(exit){
+            System.out.println("HELLO");
+            System.out.print("EXIT?");
         }
     }
 }
